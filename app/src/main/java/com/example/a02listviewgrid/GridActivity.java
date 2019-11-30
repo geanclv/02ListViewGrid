03e61858e9fc2fc5b1676b0f06a9021aef379f9f
geanclv <geanclv@gmail.com>
2019-11-30 14:13:18 -0500

    Sesión 54: - Agregando botón en el Action Bar para agregar items al GridView.

--- app/src/main/java/com/example/a02listviewgrid/GridActivity.java
+++ app/src/main/java/com/example/a02listviewgrid/GridActivity.java
@@ -1,8 +1,12 @@
 package com.example.a02listviewgrid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -15,6 +19,10 @@ public class GridActivity extends AppCompatActivity {
 
     private GridView gridView;
     private List<String> lstNombre;
+
+    private MyAdapter myAdapter;
+
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +40,37 @@ public class GridActivity extends AppCompatActivity {
             }
         });
 
-        MyAdapter myAdapter = new MyAdapter(this, R.layout.grid_item, lstNombre);
+        myAdapter = new MyAdapter(this, R.layout.grid_item, lstNombre);
         gridView.setAdapter(myAdapter);
+    }
+
+    /*Para trabajar con Menús en la barra de opciones:
+    * 1. En la carpeta res: Crear un Android resource directory del tipo: menu
+    * 2. En el directorio creado: Agregar un Menu resource file
+    * 3. Modificar a gusto el Menu resource file
+    * 4. Sobreescribir los métodos onCreateOptionsMenu y onOptionsItemSelected
+    * */
+
+    //Creamos el menú
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    //Cuando hacemos clic en uno de nuestros menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                this.lstNombre.add("Agregado N°: " + (++contador));
+                //Notificando al adaptador que la data ha cambiado
+                this.myAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private List<String> initNamesList() {
@@ -41,16 +78,6 @@ public class GridActivity extends AppCompatActivity {
             add("Geancarlo");
             add("Alejandro");
             add("Juan");
-            add("Jose");add("Geancarlo");
-            add("Alejandro");
-            add("Juan");
-            add("Jose");add("Geancarlo");
-            add("Alejandro");
-            add("Juan");
-            add("Jose");add("Geancarlo");
-            add("Alejandro");
-            add("Juan");
-            add("Jose");
         }};
     }
 
